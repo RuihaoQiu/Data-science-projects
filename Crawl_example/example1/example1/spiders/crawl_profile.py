1d0b8638e1fcd6946364e07c4cf1e39e1880d696
RuihaoQiu <qiuruihao@gmail.com>
2018-05-25 16:45:20 +0200

    fini crawl profile

--- Crawl_example/example1/example1/spiders/crawl_profile.py
+++ Crawl_example/example1/example1/spiders/crawl_profile.py
@@ -1,31 +1,47 @@
 ## crawl profiles from website
 
 import scrapy
+from scrapy.http import Request, FormRequest
+from scrapy.contrib.spiders.init import InitSpider
+from time import sleep
+from random import randint
 
 ## extract list of name
 
-class ProfileSpider(scrapy.Spider):
+class ProfileSpider(InitSpider):
   name = "profile"
-#  start_urls = ['https://www.malt.fr/']
-  start_urls = ['https://www.malt.fr/s?q=%20&f-fam=&f-cat=&location=Paris&city=Paris&lat=48.85661400000001&lon=2.3522219000000177']
-#  payload = {
-#	"email": "ruihqiu@gmail.com", 
-#	"password": "8888888"}
+  allowed_domains = ['malt.fr']
+  login_url = 'https://www.malt.fr/signin'
+  start_urls = ['https://www.malt.fr/s?q=%20&f-fam=&f-cat=&location=Paris&city=Paris&lat=48.85661400000001&lon=2.3522219000000177&p=7']
 
-#  def login(self, response):
-#    return scrapy.FormRequest.from_response(
-#        response,
-#        formdata=payload,
-#        callback=self.parse)
+  def init_request(self):
+    return Request(url=self.login_url, callback=self.login)
+
+  def login(self, response):
+    return FormRequest.from_response(
+        response,
+        formcss='form.sigin-new__signin-form',
+        formdata={"j_username": "mangoboncourage@gmail.com", "j_password": "******"},
+        callback=self.check_login)
+
+  def check_login(self, response):
+    if b"Mango" in response.body:
+      self.logger.error("Login succeed!")
+      return self.initialized()
+    else:
+      self.logger.error("Login failed!")
+
 
   def parse(self, response):
-    profiles = response.css('div.profile-card-v3-footer .button.clear::attr(href)').extract()
-    for p in profiles:
+    for p in response.css('div.profile-card-v3-footer .button.clear::attr(href)'):
+      sleep(randint(0, 9))
       yield response.follow(p, self.parse_profile)
-
+     
+    for href in response.css('li a.results-pager__control::attr(href)'):
+      sleep(randint(0, 9))
+      yield response.follow(href, callback=self.parse)     
+  
   def parse_profile(self, response):
     yield { 'title': response.css('title::text').extract_first(),
 			'description': response.css('div.markdown.secondary::text').extract()}
-
-
 
